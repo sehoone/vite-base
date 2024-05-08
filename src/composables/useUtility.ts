be5ebbdde53b303eff07036c3fefbf1c836ebe32
SeHoon <sehoon1989@gmail.com>
2024-05-09 00:54:35 +0900

    hotfix/js-to-ts

--- src/composables/useUtility.ts
+++ src/composables/useUtility.ts
@@ -1,7 +1,7 @@
 /**
  * Helper function to return the module or Vue component name
  */
-export default (importMeta) => {
+export default (importMeta: any) => {
   // import.meta.url example (development): http://[::1]:5173/resources/js/components/InputHelpers/ImageGeneration.vue?t=1687718064846
   // import.meta.url example (production): https://chatgpt.ivan-lim.com/build/assets/ImageGeneration-0317397d.js
   const currentFileName = importMeta.url.match(/[^/]+\.(js|vue)/i)[0];
