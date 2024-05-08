d38f6412e52d158ef437478049436023f0d70f70
SeHoon <sehoon1989@gmail.com>
2024-05-09 00:52:47 +0900

    feature/fix-transition-effect

--- src/views/sample/TransitionTest.vue
+++ src/views/sample/TransitionTest.vue
@@ -82,16 +82,18 @@ small {
   transform: translateX(-100%);
 } */
 .slide-fade-enter-active {
-  transition: all 0.3s ease-out;
+  transition: all 0.2s ease-out;
 }
 
 .slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+  transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
 }
-
-.slide-fade-enter-from,
 .slide-fade-leave-to {
-  transform: translateX(20px);
+  transform: translateY(100px);
+  opacity: 0;
+}
+.slide-fade-enter-from {
+  transform: translateY(20px);
   opacity: 0;
 }
 </style>
