85bc88d83e12cf9e279e83c497b1c27aa4bcb40a
SeHoon <sehoon1989@gmail.com>
2024-11-29 02:19:59 +0900

    feature/cleanup

--- src/views/sample/form/_components/FormStep.vue
+++ src/views/sample/form/_components/FormStep.vue
@@ -7,12 +7,12 @@
 <script setup lang="ts">
 import { computed, inject } from 'vue';
 
-// ref injected from FormWizard
+// FormWizard에서 주입된 ref
 const currentIdx = (inject('STEP_COUNTER') as { value: number }).value++;
-// Grabs the live ref to the current form active step
+// 현재 활성화된 폼 스텝의 라이브 ref를 가져옴
 const formStepIdx = inject<{ value: number }>('CURRENT_STEP_INDEX');
 
-// If this step should be shown
+// 이 스텝이 표시되어야 하는지 여부
 const shouldShow = computed(() => {
   return formStepIdx ? currentIdx === formStepIdx.value : false;
 });
--- src/views/sample/form/_components/FormWizerd.vue
+++ src/views/sample/form/_components/FormWizerd.vue
@@ -21,53 +21,53 @@ const props = defineProps({
     required: true
   }
 });
-
+// 이벤트를 부모 컴포넌트로 전달하기 위한 emit 정의
 const emit = defineEmits(['submit']);
 const currentStepIdx = ref(0);
 
-// Injects the starting step, child <form-steps> will use this to generate their ids
+// 시작 스텝을 자식 <form-steps> 컴포넌트에 주입
 const stepCounter = ref(0);
 provide('STEP_COUNTER', stepCounter);
 
-// Inject the live ref of the current index to child components
-// will be used to toggle each form-step visibility
+// 현재 스텝 인덱스를 자식 컴포넌트에 주입
+// 각 form-step의 가시성을 토글하는 데 사용됨
 provide('CURRENT_STEP_INDEX', currentStepIdx);
 
-// if this is the last step
+// 마지막 스텝인지 확인
 const isLastStep = computed(() => {
   return currentStepIdx.value === stepCounter.value - 1;
 });
 
-// If the `previous` button should appear
+// 'previous' 버튼이 나타나야 하는지 확인
 const hasPrevious = computed(() => {
   return currentStepIdx.value > 0;
 });
 
-// extracts the indivdual step schema
+// 개별 스텝의 검증 스키마 추출
 const currentSchema = computed(() => {
   return props.validationSchema[currentStepIdx.value];
 });
 
+// vee-validate의 useForm 훅 사용
 const { values, handleSubmit } = useForm({
-  // vee-validate will be aware of computed schema changes
+  // vee-validate가 computed 스키마 변경을 인식하도록 설정
   validationSchema: currentSchema,
-  // turn this on so each step values won't get removed when you move back or to the next step
+  // 다음 스텝으로 이동하거나 이전 스텝으로 돌아갈 때 각 스텝의 값을 유지하도록 설정
   keepValuesOnUnmount: true
 });
 
-// We are using the "submit" handler to progress to next steps
-// and to submit the form if its the last step
+// 'submit' 핸들러를 사용하여 다음 스텝으로 진행하거나 마지막 스텝일 경우 폼 제출
 const onSubmit = handleSubmit((values) => {
   if (!isLastStep.value) {
     currentStepIdx.value++;
-
     return;
   }
 
-  // Let the parent know the form was filled across all steps
+  // 모든 스텝이 완료되었음을 부모에게 알림
   emit('submit', values);
 });
 
+// 이전 단계로 이동
 function goToPrev() {
   if (currentStepIdx.value === 0) {
     return;
